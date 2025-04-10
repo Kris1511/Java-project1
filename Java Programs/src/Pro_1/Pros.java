0baa4e709e7899f6acfb9f638eea260109ff7fff
Chandru-trigeminal <chandramohan@trigeminal-ai.com>
2025-04-10 12:54:07 +0530

    another project

---
+++ Java Programs/src/Pro_1/Pros.java
@@ -0,0 +1,14 @@
+package Pro_1;
+
+public class Pros {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i = 1; i < 10; i++) {
+			System.out.println("Multiplication: " + i * i);
+		}
+
+	}
+
+}
